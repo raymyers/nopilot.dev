95a430fe5b1077a163c45ae2119cb7e0367380dc
Ray Myers <rmyers@indeed.com>
2024-04-10 17:45:36 -0500

    Logo on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,10 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
+        {/* <Heading as="h1" className="hero__title">
           {siteConfig.title}
-        </Heading>
+        </Heading> */}
+        <img src="/img/nopilot-logo-simple.png" alt="nopilot.dev" style={{width: '350px', height: '350px'}} />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         {/* <div className={styles.buttons}>
           <Link
