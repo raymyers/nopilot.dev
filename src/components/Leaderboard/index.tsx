7667fb9b6d890810baece1cc291f195809f0fb09
Ray Myers <rmyers@indeed.com>
2024-05-25 15:16:15 -0500

    Update leaderboard

--- src/components/Leaderboard/index.tsx
+++ src/components/Leaderboard/index.tsx
@@ -1,6 +1,7 @@
 
 export default function Leaderboard(props): JSX.Element {
     const { compact } = props;
+    let i = 1;
     return (<div>
   <table>
     <tr>
@@ -13,8 +14,17 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <th>License</th> }
     </tr>
     <tr>
-      <td>1</td>
-      <td><a href="https://x.com/gneubig/status/1791498953709752405">OpenDevin CodeAct 1.3</a></td>
+      <td>{i++}</td>
+      <td><a href="https://aider.chat/2024/05/22/swe-bench-lite.html">Aider</a><br/> + GPT-4o + Opus</td>
+      <td>-</td>
+      <td>26.3%</td>
+      <td>Reported</td>
+      { compact || <td>Paul Gauthier</td> }
+      { compact || <td>Apache-2</td> }
+    </tr>
+    <tr>
+      <td>{i++}</td>
+      <td><a href="https://x.com/gneubig/status/1791498953709752405">OpenDevin CodeAct 1.3</a><br />+ GPT-4o</td>
       <td>-</td>
       <td>25%</td>
       <td>Reported</td>
@@ -22,7 +32,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>MIT</td> }
     </tr>
     <tr>
-      <td>2</td>
+      <td>{i++}</td>
       <td><a href="https://github.com/sweepai/sweep">Sweep</a></td>
       <td>15.7%</td>
       <td>-</td>
@@ -31,7 +41,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>Proprietary<br/>(Source Available)</td> }
     </tr>
     <tr>
-      <td>3</td>
+      <td>{i++}</td>
       <td><a href="https://press.aboutamazon.com/2024/4/aws-announces-general-availability-of-amazon-q-the-most-capable-generative-ai-powered-assistant-for-accelerating-software-development-and-leveraging-companies-internal-data">Q Developer Agent</a></td>
       <td>13.82</td>
       <td>20.33</td>
@@ -40,7 +50,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>Proprietary</td> }
     </tr>
     <tr>
-      <td>4</td>
+      <td>{i++}</td>
       <td>Devin</td>
       <td>13.48%</td>
       <td>-</td>
@@ -49,7 +59,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>Proprietary</td> }
     </tr>
     <tr>
-      <td>5</td>
+      <td>{i++}</td>
       <td><a href="https://swe-agent.com/">SWE-agent</a><br/> + GPT-4</td>
       <td>12.47%</td>
       <td>18%</td>
@@ -58,7 +68,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>MIT</td> }
     </tr>
     <tr>
-      <td>6</td>
+      <td>{i++}</td>
       <td><a href="https://github.com/nus-apr/auto-code-rover">auto-code-rover</a></td>
       <td>10.59%</td>
       <td>16.11%</td>
@@ -67,7 +77,7 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>GPL-3</td> }
     </tr>
     <tr>
-      <td>7</td>
+      <td>{i++}</td>
       <td><a href="https://twitter.com/aegucer/status/1780319507845988538">Fume</a></td>
       <td>-</td>
       <td>18.3%</td>
@@ -76,6 +86,6 @@ export default function Leaderboard(props): JSX.Element {
       { compact || <td>Proprietary</td> }
     </tr>
   </table>
-  <p><i>Last updated: 2024-05-17</i></p>
+  <p><i>Last updated: 2024-05-25</i></p>
 </div>)
 }
