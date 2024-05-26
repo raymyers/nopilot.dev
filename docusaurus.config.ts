e696c615a4c6863a2552c703686e9e8e439b5e63
Ray Myers <rmyers@indeed.com>
2024-05-26 16:49:32 -0500

    Show all past blog posts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,6 +42,8 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
